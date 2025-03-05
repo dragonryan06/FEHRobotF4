f1feca9883402f47b52c9acc27d64c4f38e9e02b
dragonryan06 <dragonryan06@gmail.com>
2025-03-05 08:45:28 -0500

    Lay out instruction sequence for milestone

--- src/main.cpp
+++ src/main.cpp
@@ -7,39 +7,47 @@
 
 #include "core/utility.h"
 #include "core/movement.h"
+#include "sensing/light.h"
 
-#define VERSION_STR "v1.1.3 Milestone 1"
+#define VERSION_STR "v1.2.0 Milestone 2"
 
 StateMachine stateMachine;
+LightDetector lightDetector;
 
 /**
- * Starting with the robot's back to the far right side of the 
- * outdoor area, drives in a straight line until it reaches 
- * the far left wall, then returns.
+ * Waits for the start light, then drives to the upper
+ * level and positions itself at the humidifier light.
  */
-void cue1() 
+void cue1()
 {
-    stateMachine.drive(32, 27);
+    LCD.WriteLine("CUE 1: Waiting for light");
+    
 }
 
 /**
- * Starting with the robot's front facing the ramp, drives all of
- * the way up until no drive surfaces are touching the ramp once
- * again.
+ * Reads and displays the color of the humidifier light, 
+ * then pushes the corresponding button in front of it.
  */
-void cue2() 
+void taskHumidifier()
 {
-    stateMachine.drive(32, 24.5);
+
 }
 
 /**
- * (BONUS STEP)
- * Starting at the top of the ramp, goes back down to the bottom of the
- * ramp, once again with no drive surfaces touching the ramp.
+ * Drives back to the lower level and positions itself 
+ * in the start box, ready to press the final button.
  */
-void cue3() 
+void cue2()
 {
-    stateMachine.drive(-32, 24.5);
+
+}
+
+/**
+ * When aligned with the stop button, presses it.
+ */
+void taskStop()
+{
+
 }
 
 int main(void)
@@ -51,19 +59,17 @@ int main(void)
     waitForTouch();
     LCD.Clear(BLACK);
 
-    LCD.WriteRC("Executing CUE 1...",0,0);
+    LCD.WriteLine("Executing CUE 1");
     cue1();
-    LCD.WriteRC("done",0,18);
+    
+    LCD.WriteLine("Executing TASK Humidifier");
+    taskHumidifier();
 
-    breakpoint();
-
-    LCD.WriteRC("Executing CUE 2...",1,0);
+    LCD.WriteLine("Executing CUE 2");
     cue2();
-    LCD.WriteRC("done",1,18);
 
-    LCD.WriteRC("Executing CUE 3...",2,0);
-    cue3();
-    LCD.WriteRC("done",2,18);
+    LCD.WriteLine("Pressing STOP!");
+    taskStop();
 
 	return 0;
 }
